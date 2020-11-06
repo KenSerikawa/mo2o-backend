8f33da6155b8f56a7c3388d6b539a0a917c3412c
KenSerikawa <kenserikawalucio@gmail.com>
2020-11-06 16:36:56 +0100

    add beer details, parse and extract them in a readable way thanks to lambdish/phunctional package

--- src/Api/ApiClient.php
+++ src/Api/ApiClient.php
@@ -30,6 +30,15 @@ final class ApiClient implements ApiClientInterface
         return $this->parseBeersResponse($beers);
     }
 
+    public function beerDetails(string $id)
+    {
+        $res = $this->client->get("beers/{$id}");
+       
+        $beer = json_decode($res->getBody()->getContents(), true);
+
+        return $this->parseBeerDetailsResponse($beer);
+    }
+
     private function prepareQueryParameters(?string $query)
     {
         if (null !== $query) {
@@ -50,6 +59,13 @@ final class ApiClient implements ApiClientInterface
         }, $beers);
     }
 
+    private function parseBeerDetailsResponse(array $beers)
+    {
+        return map(function($beer) {
+            return $this->extractBeerDetails($beer);
+        }, $beers);
+    }
+
     private function extractCommonBeerProperties(array $beer)
     {
         return [
@@ -57,5 +73,17 @@ final class ApiClient implements ApiClientInterface
             'name'        => get('name', $beer),
             'description' => get('description', $beer)
         ];
+    }
+
+    private function extractBeerDetails(array $beer)
+    {
+        return array_merge(
+            $this->extractCommonBeerProperties($beer),
+            [
+                'tagline'        => get('tagline', $beer),
+                'image_url'      => get('image_url', $beer),
+                'first_brewed'   => get('first_brewed', $beer),
+            ]
+        );
     }
 }
