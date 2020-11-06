18f808ab71a124a23feb817fb0588467dcda06cb
KenSerikawa <kenserikawalucio@gmail.com>
2020-11-06 15:38:59 +0100

    add interface implementation and guzzle client on constructor

--- src/Api/ApiClient.php
+++ src/Api/ApiClient.php
@@ -5,19 +5,21 @@ declare(strict_types=1);
 namespace App\Api;
 
 use GuzzleHttp\Client;
+use App\Api\ApiClientInterface;
+use GuzzleHttp\ClientInterface;
 
-final class ApiClient 
+final class ApiClient implements ApiClientInterface
 {
     /** @var Client */
     private $client; 
 
-    public function __construct($client)
+    public function __construct(ClientInterface $client)
     {
         $this->client = $client;
     }
 
     public function beers(string $query)
     {
-    
+
     }
 }
