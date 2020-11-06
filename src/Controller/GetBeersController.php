6ffbe665c56ac6c6eb5c8c2fd278fd9df721bd2e
KenSerikawa <kenserikawalucio@gmail.com>
2020-11-06 16:10:05 +0100

    add strict types on method arguments and return json response of client service response

--- src/Controller/GetBeersController.php
+++ src/Controller/GetBeersController.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Api\ApiClientInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
@@ -11,13 +12,17 @@ final class GetBeersController
 {
     private $client;
 
-    public function __construct($client)
+    public function __construct(ApiClientInterface $client)
     {
         $this->client = $client;
     }
 
-    public function __invoke(Request $request)
+    public function __invoke(Request $request): JsonResponse
     {
-        
+        return new JsonResponse(
+            $this->client->beers(
+                $request->get('food')
+            )
+        );
     }
 } 
